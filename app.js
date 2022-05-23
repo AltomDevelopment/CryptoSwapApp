ac699769e107e68fbe08bc2a1fbde4b3baf85711
Zachery <altomdevelopment@gmail.com>
2022-05-23 17:57:41 -0400

    added js to menu buttons

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ function showMenu () {
     let button = document.querySelector(".dropdownmenu");
     button.classList.toggle("active");
 }
+//function displays div when moused over the button 
+// add functionality to hide the div when mouse is not over the now visible div 
+var networkbutton = document.querySelector(".networkbutton");
+networkbutton.addEventListener("mouseover", displayDiv(), false);
+
+function displayDiv () {
+    let selectnetworkmenu = document.querySelector(".selectnetworkmenu");
+    selectnetworkmenu.setAttribute("style", "display: block;");
+}
