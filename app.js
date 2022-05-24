67899b3280a279dab207fe975956dc78ab229819
Altomdevelopment <zack.altom@outlook.com>
2022-05-24 17:58:16 -0400

    added list of methods that need to be created

--- app.js
+++ app.js
@@ -21,4 +21,24 @@ function closeWalletConnector() {
     button.classList.remove("active")
 }
 
-//git hub commit test2
+
+function connectMetaMask(){
+    //Check to see if extension is installed
+    
+    //if not installed redirect the user to the metamask website to install
+  };
+  
+//chain id 31337
+
+//Connecting to the MetaMask Wallet
+//The first thing we need to do in our Dapp is to connect to our MetaMask Wallet.
+
+//1We need to create a function to see if the MetaMask Chrome extension is installed
+//2If MetaMask is not installed we:
+//-Change our connectButton to Click here to install MetaMask
+//-When clicking that button it should take us to a page that will allow us to install the extension
+//-Disable the button
+//3If MetaMask is installed we:
+//-Change our connectButton to Connect
+//-When clicking that button it should allow us to connect to our MetaMask wallet
+//-Disable the button
