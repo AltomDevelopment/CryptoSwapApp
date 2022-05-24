2e25b23baa1e8541155b6587f00ad20dbddc0e50
Zachery <altomdevelopment@gmail.com>
2022-05-23 20:27:04 -0400

    added connect wallet popup and more JS

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ function showMenu () {
     let button = document.querySelector(".dropdownmenu");
     button.classList.toggle("active");
 }
-//function displays div when moused over the button 
-// add functionality to hide the div when mouse is not over the now visible div 
+
 var networkbutton = document.querySelector(".networkbutton");
 networkbutton.addEventListener("mouseover", displayDiv(), false);
 
 function displayDiv () {
     let selectnetworkmenu = document.querySelector(".selectnetworkmenu");
     selectnetworkmenu.setAttribute("style", "display: block;");
+}
+
+function displayWalletConnector() {
+    let button = document.querySelector(".walletconnector");
+    button.classList.toggle("active");
+}
+
+function closeWalletConnector() {
+    let button = document.querySelector(".walletconnector");
+    button.classList.remove("active")
 }
